e5e8adafe8405c37b90fc3e8b8d77e33b79aa75f
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-10-04 20:05:52 +0200

    Sample files

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public final class HelloWorld {
+
+    private HelloWorld() {
+
+    }
+
+    public static void main(final String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
